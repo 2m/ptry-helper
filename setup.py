6d6f70d59839cd7e07b91f08e60646fdf7e699aa
Martynas Mickevičius <self@2m.lt>
2020-10-08 13:25:33 +0300

    Remove hash

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import subprocess
 
 setup(
     name="ptry-helper",
-    version=subprocess.check_output(["git", "describe", "--tags", "--long"]).decode("utf8").strip(),
+    version=subprocess.check_output(["git", "describe", "--tags", "--long"]).decode("utf8").split("-g")[0].replace("-", "."),
     zip_safe=False,
     include_package_data=True,
     python_requires='>=3.8',
